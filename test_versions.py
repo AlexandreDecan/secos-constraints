5493fd42fa281fd41b81bbfacbbdcfe06828e773
Alexandre Decan <alexandre.decan@lexpage.net>
2018-04-02 12:57:57 +0200

    Initial commit

---
+++ test_versions.py
@@ -0,0 +1,125 @@
+import pytest
+from versions import Version, Interval, IntervalSet
+
+
+def test_create_versions():
+    assert Version('1.2.3') == Version(1, 2, 3)
+    assert Version('v1.2.3') == Version(1, 2, 3)
+    assert Version('V1.2.3') == Version(1, 2, 3)
+    assert Version('1.2.3-pre') == Version(1, 2, 3, '-pre')
+    assert Version('1.2.3.4.5') == Version(1, 2, 3, '.4.5')
+    
+    failures = ['a', '1', '1.2', '1a.2.3', '']
+    for failure in failures:
+        with pytest.raises(ValueError):
+            Version(failure)
+            
+    
+def test_versions_comparison():
+    pairs = [
+        ('1.0.0', '2.0.0'),
+        ('1.0.0', '1.1.0'),
+        ('1.0.0', '1.0.1'),
+        ('1.0.0-pre', '1.0.0'),
+    ]
+    
+    for v1, v2 in pairs:
+        v1, v2 = Version(v1), Version(v2)
+        
+        assert v1 == v1
+        assert v2 == v2
+        assert v1 != v2
+        assert v1 < v2
+        assert v1 <= v2
+        assert v2 > v1
+        assert v2 >= v1
+        
+        assert Version.lowest() < v1 < Version.highest()
+
+
+def test_range_containment():
+    v1, v2, v3, v4 = Version('1.0.0'), Version('2.0.0'), Version('3.0.0'), Version('4.0.0')
+    
+    assert Interval.closed(v2, v3) in Interval.closed(v1, v4)
+    assert v2 in Interval.closed(v1, v3)
+    assert v2 in Interval.closed(v2, v3)
+    assert v3 in Interval.closed(v2, v3)
+    
+    assert Interval.open(v1, v2) in Interval.closed(v1, v2)
+    
+    assert Interval.closed(v1, v2) in Interval.closed(v1, Version.highest())
+    assert v1 in Interval.closed(v1, Version.highest())
+        
+    assert v1 not in Interval.open(v1, v2)
+    assert v2 not in Interval.open(v1, v2)
+    
+    
+def test_range_and():
+    v1, v2, v3, v4 = Version('1.0.0'), Version('2.0.0'), Version('3.0.0'), Version('4.0.0')
+    
+    assert Interval.closed(v1, v3) & Interval.closed(v2, v4) == Interval.closed(v2, v3)
+    assert Interval.closed(v1, Version.highest()) & Interval.closed(v2, v4) == Interval.closed(v2, v4)
+    assert Interval.closed(Version.lowest(), v2) & Interval.closed(v1, v4) == Interval.closed(v1, v2)
+    
+    assert Interval.closed(v1, v2) & Interval.closed(v1, v2) == Interval.closed(v1, v2)
+    assert Interval.closed(v1, v2) & Interval.closed(v2, v3) == Interval.closed(v2, v2)
+    
+    
+def test_range_is_empty():
+    v1, v2, v3, v4 = Version('1.0.0'), Version('2.0.0'), Version('3.0.0'), Version('4.0.0')
+    
+    assert not Interval.closed(v1, v2).empty()
+    assert not Interval.closed(v1, v1).empty()
+    
+    assert Interval.openclosed(v1, v1).empty()
+    assert Interval.closedopen(v1, v1).empty()
+    assert (Interval.closed(v1, v2) & Interval.closed(v3, v4)).empty()
+
+
+def test_range_intersection():
+    v1, v2, v3, v4 = Version('1.0.0'), Version('2.0.0'), Version('3.0.0'), Version('4.0.0')
+    
+    assert (Interval.closed(v1, v2) & Interval.closed(v3, v4)).empty()
+    assert Interval.closed(v1, v2) & Interval.closed(v1, v2) == Interval.closed(v1, v2)
+    
+    assert Interval.open(v1, v2) & Interval.closed(v1, v2) == Interval.open(v1, v2)
+    assert Interval.closed(v1, v3) & Interval.open(v2, v4) == Interval.openclosed(v2, v3)
+
+
+def test_range_union():
+    v1, v2, v3, v4 = Version('1.0.0'), Version('2.0.0'), Version('3.0.0'), Version('4.0.0')
+    
+    assert Interval.closed(v1, v2) | Interval.closed(v1, v2) == Interval.closed(v1, v2)
+    assert Interval.closed(v1, v4) | Interval.closed(v2, v3) == Interval.closed(v1, v4)
+    
+    assert Interval.closed(v1, v2) | Interval.open(v2, v3) == Interval.closedopen(v1, v3)
+    assert Interval.closed(v1, v3) | Interval.closed(v2, v4) == Interval.closed(v1, v4)
+    
+    assert Interval.closedopen(v1, v2) | Interval.closed(v2, v3) == Interval.closed(v1, v3)
+    assert Interval.open(v1, v2) | Interval.closed(v2, v4) == Interval.openclosed(v1, v4)
+    
+    assert Interval.closed(v1, v2) | Interval.closed(v3, v4) == IntervalSet(Interval.closed(v1, v2), Interval.closed(v3, v4))
+
+
+def test_set_intersection():
+    v1, v2, v3, v4, v5, v6 = Version('1.0.0'), Version('2.0.0'), Version('3.0.0'), Version('4.0.0'), Version('5.0.0'), Version('6.0.0')
+    
+    is1 = IntervalSet(Interval.closed(v1, v3), Interval.closed(v4, v6))
+    is2 = IntervalSet(Interval.closed(v2, v4), Interval.open(v5, v6))
+    is3 = IntervalSet(Interval.closed(v2, v3), Interval.closed(v4, v4), Interval.open(v5, v6))
+
+    assert is1 & is2 == is3
+    
+    
+def test_set_union():
+    v1, v2, v3, v4 = Version('1.0.0'), Version('2.0.0'), Version('3.0.0'), Version('4.0.0')
+    
+    assert Interval.closed(v1, v2) | Interval.closed(v3, v4) == IntervalSet(Interval.closed(v1, v2), Interval.closed(v3, v4))
+    assert Interval.closed(v1, v2) | Interval.closed(v3, v4) | Interval.closed(v2, v3) == IntervalSet(Interval.closed(v1, v4))
+    
+
+def test_set_to_interval():
+    v1, v2, v3, v4 = Version('1.0.0'), Version('2.0.0'), Version('3.0.0'), Version('4.0.0')
+    
+    assert IntervalSet(Interval.openclosed(v2, v3)).to_interval() == Interval.openclosed(v2, v3)
+    assert IntervalSet(Interval.open(v1, v2), Interval.closed(v3, v4)).to_interval() == Interval.openclosed(v1, v4)
---
+++ versions.py
@@ -0,0 +1,356 @@
+import re
+from functools import total_ordering
+from itertools import combinations
+
+
+@total_ordering
+class Version:
+    """
+    Represent a three-component (+ optional prerelease tags) based version.
+    If a string is provided, try to parse it using Version.from_string.
+    """
+
+    RE = r'^(?:v|V)?(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)(?P<misc>.+)?$'
+    
+    def __init__(self, major_or_str, minor=None, patch=None, misc=None):
+        if isinstance(major_or_str, str):
+            version = Version.from_string(major_or_str)
+            major_or_str = version.major
+            minor = version.minor
+            patch = version.patch
+            misc = version.misc
+        
+        if isinstance(major_or_str, int) and isinstance(minor, int) and isinstance(patch, int):
+            self.major = major_or_str
+            self.minor = minor
+            self.patch = patch
+            self.misc = misc
+        else:
+            raise ValueError('Major, minor and patch components must be integers.')
+        
+    @staticmethod
+    def from_string(string):
+        """
+        Parse given string and return a Version instance.
+        """
+        match = re.fullmatch(Version.RE, string)
+        if match is not None:
+            v = match.groupdict()
+            return Version(int(v['major']), int(v['minor']), int(v['patch']), v['misc'])
+        else:
+            raise ValueError('Cannot parse {}'.format(string))
+            
+    @staticmethod
+    def lowest():
+        """
+        Lowest possible version.
+        """
+        v = Version('0.0.0')
+        v.major = float('-inf')
+        v.__repr__ = lambda s: '-inf'
+        
+        return v
+        
+    @staticmethod
+    def highest():
+        """
+        Highest possible version.
+        """
+        v = Version('0.0.0')
+        v.major = float('inf')
+        v.__repr__ = lambda s: 'inf'
+        
+        return v
+            
+    def __eq__(self, other):
+        if isinstance(other, Version):
+            return (
+                self.major == other.major
+                and self.minor == other.minor
+                and self.patch == other.patch
+                and self.misc == other.misc
+            )
+        else:
+            return NotImplemented
+    
+    def __hash__(self):
+        return hash(self.minor) + hash(self.minor) + hash(self.patch)
+    
+    def __lt__(self, other):
+        if isinstance(other, Version):
+            if self.major < other.major:
+                return True
+            elif self.major == other.major:
+                if self.minor < other.minor:
+                    return True
+                elif self.minor == other.minor:
+                    if self.patch < other.patch:
+                        return True
+                    elif self.patch == other.patch:
+                        if self.misc and other.misc:
+                            return self.misc < other.misc
+                        else:
+                            # prereleases are always before non-prereleases
+                            return self.misc and not other.misc
+            return False
+        else:
+            return NotImplemented
+    
+    def __repr__(self):
+        return '{}.{}.{}{}'.format(self.major, self.minor, self.patch, self.misc if self.misc else '')
+
+
+class Interval:
+    """
+    Represent an (open/closed) interval.
+    Support intersection (&) and union (|).
+    
+    Inspiration taken from https://github.com/intiocean/pyinter
+    """
+    OPEN = 0
+    CLOSED = 1
+    
+    def __init__(self, left, lower, upper, right):
+        if lower > upper:
+            raise ValueError('Bounds are not ordered correctly: {} must be before {}'.format(lower, upper))
+        self.left = left
+        self.lower = lower
+        self.upper = upper
+        self.right = right
+
+    @staticmethod
+    def open(lower, upper):
+        return Interval(Interval.OPEN, lower, upper, Interval.OPEN)
+        
+    @staticmethod
+    def closed(lower, upper):
+        return Interval(Interval.CLOSED, lower, upper, Interval.CLOSED)
+        
+    @staticmethod
+    def openclosed(lower, upper):
+        return Interval(Interval.OPEN, lower, upper, Interval.CLOSED)
+        
+    @staticmethod
+    def closedopen(lower, upper):
+        return Interval(Interval.CLOSED, lower, upper, Interval.OPEN)
+
+    def empty(self):
+        return self.lower == self.upper and (self.left == Interval.OPEN or self.right == Interval.OPEN)
+
+    def __eq__(self, other):
+        if isinstance(other, Interval):
+            return (
+                self.left == other.left and
+                self.lower == other.lower and
+                self.upper == other.upper and
+                self.right == other.right
+            )
+        else:
+            return NotImplemented
+
+    def overlaps(self, other, contigous=False):
+        """
+        Return True if sets have any overlapping value.
+        If 'contigous' is set to True, it considers [1, 2) and [2, 3] as an
+        overlap on value 2, not [1, 2) and (2, 3].
+        """
+        if self.lower > other.lower:
+            first, second = other, self
+        else:
+            first, second = self, other
+         
+        # if second.empty():
+        #    return False
+         
+        if first.upper == second.lower:
+            if contigous:
+                return first.right == Interval.CLOSED or second.right == Interval.CLOSED
+            else:
+                return first.right == Interval.CLOSED and second.right == Interval.CLOSED
+         
+        return first.upper > second.lower
+
+    def __and__(self, other):
+        if isinstance(other, Interval):
+            if self.lower == other.lower:
+                lower = self.lower
+                left = self.left if self.left == Interval.OPEN else other.left
+            else:
+                lower = max(self.lower, other.lower)
+                left = self.left if lower == self.lower else other.left
+                
+            if self.upper == other.upper:
+                upper = self.upper
+                right = self.right if self.right == Interval.OPEN else other.right
+            else:
+                upper = min(self.upper, other.upper)
+                right = self.right if upper == self.upper else other.right
+            
+            if lower <= upper:
+                return Interval(left, lower, upper, right)
+            else:
+                return Interval(Interval.OPEN, lower, lower, Interval.OPEN)
+        else:
+            return NotImplemented
+
+    def __or__(self, other):
+        if isinstance(other, Interval):
+            if self.overlaps(other, contigous=True):
+                if self.lower == other.lower:
+                    lower = self.lower
+                    left = self.left if self.left == Interval.OPEN else other.left
+                else:
+                    lower = min(self.lower, other.lower)
+                    left = self.left if lower == self.lower else other.left
+                    
+                if self.upper == other.upper:
+                    upper = self.upper
+                    right = self.right if self.right == Interval.OPEN else other.right
+                else:
+                    upper = max(self.upper, other.upper)
+                    right = self.right if upper == self.upper else other.right
+                
+                return Interval(left, lower, upper, right)
+            else:
+                return IntervalSet(self, other)
+        else:
+            return NotImplemented
+
+    def __hash__(self):
+        return hash(self.lower)
+        
+    def _contains_value(self, value):
+        left = (value >= self.lower) if self.left == Interval.CLOSED else (value > self.lower)
+        right = (value <= self.upper) if self.right == Interval.CLOSED else (value < self.upper)
+        return left and right
+            
+    def __contains__(self, item):
+        if isinstance(item, Interval):
+            left = item.lower > self.lower or (item.lower == self.lower and (item.left == self.left or self.left == Interval.CLOSED))
+            right = item.upper < self.upper or (item.upper == self.upper and (item.right == self.right or self.right == Interval.CLOSED))
+            return left and right
+        else:
+            return self._contains_value(item)
+
+    def __repr__(self):
+        return '{}{}:{}{}'.format(
+            '[' if self.left == Interval.CLOSED else ']',
+            repr(self.lower),
+            repr(self.upper),
+            ']' if self.right == Interval.CLOSED else '[',
+        )
+
+
+class IntervalSet:
+    """
+    Disjunction of intervals.
+    """
+    def __init__(self, interval, *intervals):
+        self.intervals = set()
+        
+        self.intervals.add(interval)
+        for interval in intervals:
+            self.intervals.add(interval)
+        
+        self.clean()
+
+    def clean(self):
+        # Remove empty intervals
+        self.intervals = {i for i in self.intervals if not i.empty()}
+        
+        # Remove intervals contained in other ones
+        to_remove = set()
+        for i1, i2 in combinations(self.intervals, 2):
+            if i1 not in to_remove and i1 in i2:
+                to_remove.add(i1)
+        self.intervals = self.intervals.difference(to_remove)
+        
+        # Merge contigous intervals
+        to_remove = set()
+        to_add = set()
+        for i1, i2 in combinations(self.intervals, 2):
+            if i1 not in to_remove and i2 not in to_remove and i1.overlaps(i2, contigous=True):
+                # Merge i1 and i2
+                i3 = i1 | i2
+                assert isinstance(i3, Interval)
+                to_remove.add(i1)
+                to_remove.add(i2)
+                to_add.add(i3)
+                
+        # Do until nothing change
+        self.intervals = self.intervals.difference(to_remove).union(to_add)
+        if len(to_remove) + len(to_add) > 0:
+            self.clean()
+            
+    def empty(self):
+        return len(self.intervals) == 0
+
+    def to_interval(self):
+        first = next(iter(self.intervals))
+        
+        lower = first.lower
+        left = first.left
+        upper = first.upper
+        right = first.right
+        
+        for interval in self.intervals:
+            if interval.lower < lower:
+                lower = interval.lower
+                left = interval.left
+            elif interval.lower == lower:
+                if left == Interval.OPEN and interval.left == Interval.CLOSED:
+                    left = Interval.CLOSED
+                    
+            if interval.upper > upper:
+                upper = interval.upper
+                right = interval.right
+            elif interval.upper == upper:
+                if right == Interval.OPEN and interval.right == Interval.CLOSED:
+                    right = Interval.CLOSED
+        
+        return Interval(left, lower, upper, right)
+
+    def __len__(self):
+        return len(self.intervals)
+    
+    def __iter__(self):
+        return iter(self.intervals)
+    
+    def __containts__(self, item):
+        for i in self.intervals:
+            if item in i:
+                return True
+        return False
+        
+    def __and__(self, other):
+        if isinstance(other, (Interval, IntervalSet)):
+            if isinstance(other, Interval):
+                intervals = [other]
+            else:
+                intervals = list(other.intervals)
+            new_intervals = []
+            for interval in self.intervals:
+                for o_interval in intervals:
+                    new_intervals.append(interval & o_interval)
+            return IntervalSet(*new_intervals)
+        else:
+            return NotImplemented
+        
+    def __or__(self, other):
+        if isinstance(other, Interval):
+            return self | IntervalSet(other)
+        elif isinstance(other, IntervalSet):
+            return IntervalSet(*(list(self.intervals) + list(other.intervals)))
+        else:
+            return NotImplemented
+    
+    def __eq__(self, other):
+        if isinstance(other, Interval):
+            return IntervalSet(other) == self
+        elif isinstance(other, IntervalSet):
+            return self.intervals == other.intervals
+        else:
+            return NotImplemented
+    
+    def __repr__(self):
+        return ' | '.join(repr(i) for i in self.intervals)
